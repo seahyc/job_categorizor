10e5a499f42b7008aca7f1c25bc1fde3507138ce
Ubuntu <ubuntu@ip-172-31-28-165.ap-southeast-1.compute.internal>
2015-11-12 15:34:48 +0000

    local app python mods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 
 from flask import Flask, jsonify, request, abort, make_response
 import analyze
@@ -84,4 +84,4 @@ def weighting(tabulate, weightage):
 	return sorted(master_list, key=lambda item: -item['relevance'])
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=80,debug=True)
